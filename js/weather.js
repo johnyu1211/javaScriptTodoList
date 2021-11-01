7216556f2362bdc93922793a8769dc9b43d33e84
[Github <johnyu1211@gmail.com>
2021-11-01 17:41:22 +0900

    weatherAPI end

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,23 @@
 const API_KEY = "cfc9aa2a1304dc9aa7240f12bfff5e8f"
 
-function onGeoOk(positions){
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 
-    const lat = positions.coords.latitude;
-    const lon = positions.coords.longitude;
-    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
 
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
-function onGeoError(){
-    alert("Can't find your location")
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
 }
-
-navigator.geolocation.getCurrentPosition()
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
